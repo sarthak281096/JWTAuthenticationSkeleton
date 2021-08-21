c163e904934ef1111cedb4e62a4d3796f20da0d9
sarthak goel <goelsarthak28@gmail.com>
2021-08-21 19:33:57 +0530

    initial commit

--- dbSchema/db.sql
+++ dbSchema/db.sql
@@ -4,7 +4,10 @@ use spark;
 CREATE TABLE `user` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100),
-    `email` VARCHAR(100),
+    `address` VARCHAR(1000),
+    `contact` VARCHAR(12),
+    `gender` VARCHAR(6),
+    `country` VARCHAR(100),
     `password` VARCHAR(100),
     PRIMARY KEY (`id`), 
     UNIQUE(`email`)
